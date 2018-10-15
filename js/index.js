e4f32f4044511541dd19947e2723038508e82f81
Katrina Pritchard <pritckat@gmail.com>
2018-10-15 22:51:49 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // your code here
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
-
+  const commitsList = `<ul>${commits.map(
+    commit => '<li><h3>' + commit.commit.author.name + ' (' +
+    commit.author.login + ')</h3>' + commit.commit.message + '</li>'
+  ).join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
 }
