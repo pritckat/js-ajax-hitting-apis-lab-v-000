a319d90d93c9f39badf5420368bc5b85ec36a53a
Katrina Pritchard <pritckat@gmail.com>
2018-10-15 23:08:26 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -37,5 +37,11 @@ function displayRepositories() {
 }
 
 function getRepositories() {
-
+  const name = document.getElementById('username').value;
+  const uri = rootURL + '/users/' + name + '/repos';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayRepositories);
+  xhr.open('GET', uri);
+  xhr.send();
+  return false;
 }
